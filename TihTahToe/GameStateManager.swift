e82d2141cbd9a7b9136186e43d77d974fc03913a
OwenFahringer <owenfahringer@gmail.com>
2025-05-29 07:13:00 -0400

    Started creating the smallBoard class as well as logic for figureing out which spot would be active next and worked the logic for finding a win scenario

---
+++ TihTahToe/GameStateManager.swift
@@ -0,0 +1,15 @@
+//
+//  BoardVM.swift
+//  TihTahToe
+//
+//  Created by Owen F on 5/28/25.
+//
+
+import SwiftUI
+
+class gameStateManager: ObservableObject {
+    
+    @State private var turn: Bool = false //false is x, true is o
+    
+    
+}
