4fc0daffbabc379ac69d2c2cb0d4c05d2f29c889
OwenFahringer <owenfahringer@gmail.com>
2025-05-29 07:13:23 -0400

    Started creating the smallBoard class as well as logic for figureing out which spot would be active next and worked the logic for finding a win scenario

--- TihTahToe/GameStateManager.swift
+++ TihTahToe/GameStateManager.swift
@@ -7,9 +7,17 @@
 
 import SwiftUI
 
-class gameStateManager: ObservableObject {
+class GameStateManager: ObservableObject {
+    @Published var boardGrid: [[smallBoard]] = Array(repeating: Array(repeating: smallBoard(), count: 3), count: 3)
+    @Published var turn: Bool = false //false is x, true is o
     
-    @State private var turn: Bool = false //false is x, true is o
     
-    
+    func nextActive(row: Int, col: Int){
+        for i in boardGrid.indices{
+            for j in boardGrid[i].indices{
+                boardGrid[i][j].isActive = false
+            }
+        }
+        boardGrid[row][col].isActive = true
+    }
 }
---
+++ TihTahToe/SmallBoard.swift
@@ -0,0 +1,42 @@
+//
+//  smallBoard.swift
+//  TihTahToe
+//
+//  Created by Owen F on 5/29/25.
+//
+
+import SwiftUI
+
+class smallBoard: ObservableObject {
+    @Published var board: [[String]] = Array(repeating: Array(repeating: "", count: 3), count: 3)
+    @Published var isActive: Bool = true
+    @Published var isWon: Bool = false
+    
+    
+    func addFilledTile(row: Int, col: Int, tile: String) {
+        board[row][col] = tile
+        
+    }
+    func didWin(row: Int, col: Int, tile: String) -> String{
+        if(
+        (board[row][0] == tile && board[row][1] == tile && board[row][2] == tile) || //row
+        (board[0][col] == tile && board[1][col] == tile && board[2][col] == tile) || //column
+        (board[0][0] == tile && board[1][1] == tile && board[2][2] == tile) || //top left bottom right diagonal
+        (board[2][0] == tile && board[1][1] == tile && board[0][2] == tile) //bottom left top right diagonal
+        ){
+            isActive = false; isWon = true
+            return tile
+        }
+        else{
+            return ""
+        }
+        
+    }
+}
+
+
+/*
+check all surrounding tiles
+ 
+ 
+ */
