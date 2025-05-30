748ac28a3b929cf49c86c599893690d5b92bb835
OwenFahringer <owenfahringer@gmail.com>
2025-05-30 14:09:43 -0400

    Attempted making shapes show when the tile has been clicked

--- TihTahToe/GameViews/PopupView.swift
+++ TihTahToe/GameViews/PopupView.swift
@@ -44,30 +44,45 @@ struct SmallBoardPopupView: View {
                 .frame(width: 110, height:110)
                 .foregroundStyle(.white)
             VStack {
-                ForEach(0..<3) { i in
-                    HStack {
-                        ForEach(0..<3) { j in
-                            Button{
-                                
-                            } label: {
-                                ZStack {
-                                    if gsm.largeBoard[bigBoardRow][bigBoardCol].board[i][j] == "x" {
-                                        Image("x.square")
-                                    } else if gsm.largeBoard[bigBoardRow][bigBoardCol].board[i][j] == "o" {
-                                        Image("o.square")
-                                    }
-                                    RoundedRectangle(cornerRadius: 5)
-                                        .stroke(lineWidth: 1.5)
-                                        .frame(width: 30, height: 30)
-                                        .foregroundColor(.black)
-                                        .shadow(color: .white, radius: 2)
-                                }
-                            }
-                        }
+                ForEach(0..<3, id: \.self) { i in
+                HStack(spacing: 4) {
+                    ForEach(0..<3, id: \.self) { j in
+                        CellButton(i: i, j: j, bigBoardRow: bigBoardRow, bigBoardCol: bigBoardCol)
                     }
                 }
             }
+
+            }
         }
         .padding(5)
+    }
+}
+struct CellButton: View {
+    @EnvironmentObject var gsm: GameStateManager
+    let i: Int
+    let j: Int
+    let bigBoardRow: Int
+    let bigBoardCol: Int
+
+    var body: some View {
+        let symbol = gsm.largeBoard[bigBoardRow][bigBoardCol].board[i][j]
+        let isDisabled = symbol != ""
+
+        return Button {
+            gsm.largeBoard[bigBoardRow][bigBoardCol].board[i][j] = gsm.turn
+        } label: {
+            ZStack {
+                if symbol == "x" {
+                    Image(systemName: "xmark.square")
+                } else if symbol == "o" {
+                    Image(systemName: "circle")
+                }
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(lineWidth: 1.5)
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(.black)
+            }
+        }
+        .disabled(isDisabled)
     }
 }
